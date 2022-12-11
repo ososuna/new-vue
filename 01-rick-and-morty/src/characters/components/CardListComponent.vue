324488713453d3771d960718c6ebfc5770f8ec71
oswi <oswaldo.adrian@live.com.mx>
2022-12-10 19:17:10 -0600

    use query

--- 01-rick-and-morty/src/characters/components/CardListComponent.vue
+++ 01-rick-and-morty/src/characters/components/CardListComponent.vue
@@ -1,11 +1,31 @@
 <script setup lang="ts">
-import { useCharacters } from '@/characters/composables/useCharacters';
-const { isLoading, characters, hasError, errorMessage } = useCharacters();
+import rickAndMortyApi from '@/api/rickAndMortyApi';
+// import { useCharacters } from '@/characters/composables/useCharacters';
+import { useQuery } from '@tanstack/vue-query';
+import type { Character, CharacterResponse } from '@/characters/interfaces/character';
+
+// 1. Normal suspense
+// const { data } = await rickAndMortyApi.get<Character[]>('/character');
+// const characters = data.results;
+
+// 2. Composable functions
+// const { isLoading, characters, hasError, errorMessage } = useCharacters();
+
+// 3. TanStack Query
+const getCharactersSlow = async():Promise<Character[]> => {
+  const { data } = await rickAndMortyApi.get<CharacterResponse>('/character');
+  return data.results;
+}
+const { isLoading, isError, data: characters, error } = useQuery(
+  ['characters'],
+  getCharactersSlow,
+);
+
 </script>
 
 <template>
   <h3 v-if="isLoading">Loading...</h3>
-  <h3 v-if="hasError">{{ errorMessage }}</h3>
+  <h3 v-if="isError">{{ error }}</h3>
   <ul>
     <li
       v-for="{ id, name } of characters"
--- 01-rick-and-morty/src/main.ts
+++ 01-rick-and-morty/src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { VueQueryPlugin } from '@tanstack/vue-query';
 import './assets/main.css';
 
 createApp(App)
+  .use(VueQueryPlugin)
   .use(router)
   .mount('#app');
