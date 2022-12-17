1394056b3b8c1728dd3ce5d33615154d3fb44ff0
oswi <oswaldo.adrian@live.com.mx>
2022-12-17 14:42:36 -0700

    loading character data

--- 01-rick-and-morty/src/characters/pages/CharacterIdPage.vue
+++ 01-rick-and-morty/src/characters/pages/CharacterIdPage.vue
@@ -1,8 +1,19 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
+import rickAndMortyApi from '@/api/rickAndMortyApi';
+import type { Character } from '@/characters/interfaces/character';
+import characterStore from '@/store/characters.store';
 
 const route = useRoute();
 const { id } = route.params as { id: string };
+
+const getCharacterCacheFirst = async( characterId: string ): Promise<Character> => {
+  if (characterStore.checkIdInStore( characterId )) {
+    return characterStore.ids.list[characterId];
+  }
+  const { data } = await rickAndMortyApi.get<Character>(`/character/${characterId}`);
+  return data;
+}
 
 </script>
 
