7f67a93ec9872a873fa7bf422e4f8e2191c9cd22
oswi <oswaldo.adrian@live.com.mx>
2022-12-23 23:49:21 -0700

    store clients

---
+++ 02-pinia-app/src/store/clients.ts
@@ -0,0 +1,27 @@
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
+import type { Client } from '@/clients/interfaces/client';
+
+export const useClientsStore = defineStore('clients', () => {
+
+  const currentPage = ref<number>(1);
+  const totalPages = ref<number>(5);
+  const clients = ref<Client[]>([]);
+
+  return {
+    // State
+    currentPage,
+    totalPages,
+    clients,
+    // Getters
+
+    // Actions
+    setClients( newClients: Client[] ) {
+      clients.value = newClients;
+    },
+    setPage( page: number ) {
+      if ( currentPage.value === page ) return;
+      currentPage.value = page;
+    }
+  }
+});
