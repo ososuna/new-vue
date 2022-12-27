f2d5b6f114b1a7d6c601f0ea22b8def73f05053a
oswi <oswaldo.adrian@live.com.mx>
2022-12-27 09:43:21 -0700

    issues list structure

---
+++ 03-issues-app/src/issues/pages/ListPage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import LoaderSpinnerComponent from 'src/shared/components/LoaderSpinnerComponent.vue';
+</script>
+
+<template>
+  <div class="row q-mb-md">
+    <div class="col-12">
+      <span class="text-h4">GitHub Issues</span>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12 col-md-4"></div>
+    <div class="col-xs-12 col-md-8">
+      <LoaderSpinnerComponent />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ 03-issues-app/src/shared/components/LoaderSpinnerComponent.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+  color?: string;
+  size?: string;
+  thickness?: number;
+}
+withDefaults(defineProps<Props>(), {
+  color: 'primary',
+  size: '3rem',
+  thickness: 5,
+});
+</script>
+
+<template>
+  <div class="column items-center">
+    <q-spinner :size="size" :color="color" :thickness="thickness" />
+    <span class="q-mt-sm">Loading</span>
+  </div>
+</template>
+
+<style scoped></style>
